49000092029302e21ea81b254f67b161b646bb3b
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2022-01-12 09:33:58 -0800

    Update robo500.c

--- robo500.c
+++ robo500.c
@@ -2,15 +2,14 @@ task main() {
     displayCenteredBigTextLine(3, "I use Arch BTW.");  
     for(int repeats = 0; repeats <= 8; repeats++) {
     
-        // Moves forward for 2 seconds
         motor[motorB] = 100;        
-        motor[motorC]  = 100;        
+        motor[motorC]  = 100;
+        displayCenteredBigTextLine(3, "Moving Forward.");
         wait1Msec(1980);
         
         motor[motorB] = 0;
         motor[motorC] = 100;
+        displayCenteredBigTextLine(3, "Turning Right.");
         wait1Msec(850);
-    
-    
     }                                                           
 }   
