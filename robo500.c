273892a8301c5d9136e53cbd27164117385f03cb
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2022-01-12 12:02:19 -0800

    Update robo500.c

--- robo500.c
+++ robo500.c
@@ -1,15 +1,17 @@
+#pragma platform(NXT)
+#pragma DebuggerWindows("nxtLCDScreen")
 task main() {
-    displayCenteredBigTextLine(3, "I use Arch BTW.");  
+    displayCenteredTextLine(3, "I use Arch BTW.");
     for(int repeats = 0; repeats <= 8; repeats++) {
-    
-        motor[motorB] = 100;        
+
+        motor[motorB] = 100;
         motor[motorC]  = 100;
-        displayCenteredBigTextLine(3, "Moving Forward.");
-        wait1Msec(1980);
-        
-        motor[motorB] = 0;
+        displayTextLine(0, "Moving Forward.");
+        wait1Msec(2100);
+
+        motor[motorB] = -100;
         motor[motorC] = 100;
-        displayCenteredBigTextLine(3, "Turning Right.");
-        wait1Msec(850);
-    }                                                           
-}   
+        displayTextLine(0, "Turning Right.");
+        wait1Msec(386);
+    }
+}
