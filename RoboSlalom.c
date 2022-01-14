fece4c48b704138ee8e04d7a1b23f8c268cf0ecb
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2022-01-14 08:18:46 -0800

    Update RoboSlalom.c

--- RoboSlalom.c
+++ RoboSlalom.c
@@ -1,6 +1,3 @@
-#pragma platform(NXT)
-#pragma DebuggerWindows("nxtLCDScreen")
-//very inconsistent 
 void forward(int x){
 	motor[motorB] = 100;
   motor[motorC]  = 100;
@@ -23,36 +20,27 @@ void right(int x){
 }
 
 task main() {
-    displayCenteredTextLine(3, "I use Arch BTW.");
-		right(500);
+		displayCenteredTextLine(3, "I use Arch BTW.");
+		right(230);
 		displayTextLine(1, "1");
+		forward(1650);
+		left(375);
+
 		forward(1000);
-		displayTextLine(1, "2");
-    left(390);
-    displayTextLine(1, "3");
-    forward(1700);
-    displayTextLine(1, "4");
-    left(390);
-    displayTextLine(1, "5");
-    forward(2000);
-    displayTextLine(1, "6");
-    right(400);
-    displayTextLine(1, "7");
-    forward(1400);
-    displayTextLine(1, "8");
-    right(450);
-    displayTextLine(1, "9");
-    forward(2250);
-    displayTextLine(1, "10");
-    left(400);
-    displayTextLine(1, "11");
-    forward(1100);
-    displayTextLine(1, "12");
-    left(350);
-    displayTextLine(1, "13");
-    forward(2000);
-    displayTextLine(1, "14");
-    right(320);
-    displayTextLine(1, "15");
-    forward(1000);
+		left(100);
+		forward(1300);
+		right(450);
+
+		forward(1000);
+		right(190);
+		forward(800);
+		right(20);
+		forward(500);
+		left(350);
+		forward(800);
+		left(350);
+
+		forward(2400);
+		right(400);
+		forward(1000);
 }
