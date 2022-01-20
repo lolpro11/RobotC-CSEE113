9bc597d24583d46799d855f9dc0989f922610aee
lolpro11 <lolpro11lolx@gmail.com>
2022-01-20 09:40:02 -0800

    runner

---
+++ runner1.c
@@ -0,0 +1,79 @@
+#pragma config(Sensor, S1,     touchSensor,              sensorTouch)
+#pragma config(Sensor, S2,     compassSensor,            sensorVirtualCompass)
+#pragma config(Sensor, S3,     lightSensor,              sensorLightActive)
+#pragma config(Sensor, S4,     sonarSensor,              sensorSONAR)
+#pragma config(Motor,  motorA,          gripperMotor,       tmotorNormal, PIDControl, encoder)
+#pragma config(Motor,  motorB,          rightMotor,         tmotorNormal, PIDControl, encoder)
+#pragma config(Motor,  motorC,          leftMotor,          tmotorNormal, PIDControl, encoder)
+
+/*************************************\
+|*  Programmer: Mr. Sep              *|
+|*  Class     : Intro to Robotic Eng *|
+|*  Term      : Spring 2022          *|
+|*                                   *|
+|*  Task      : Line Runner Level 1  *|
+|*  Robot     : Buggy (CSEE) Bot     *|
+\*************************************/
+
+void driveStraight(int powerLevel)
+{
+	motor[rightMotor] = powerLevel;
+	motor[leftMotor]  = powerLevel;
+}
+
+void turnRight90()
+{
+	nMotorEncoder[leftMotor] = 0;
+	while (nMotorEncoder[leftMotor] < 146)
+	{
+		motor[rightMotor] = -50;
+		motor[leftMotor]  = 50;
+	}
+	motor[leftMotor] = 0;
+}
+
+task main()
+{
+	string botName = "Buggy (CSEE) Bot";
+	int fullPower = 100;
+	int halfPower = 50;
+	int fullStop = 0;
+	int lineCount = 0;
+	bool foundBlack = false;
+
+	displayTextLine(2, "%s", botName);
+
+	while (true)
+	{
+		// move forward full power until touch sensor pressed
+		// then reverse half power and turn right and forward
+		if (SensorValue(touchSensor) == 0)
+		{
+			driveStraight(30);
+		}
+		else
+		{
+			driveStraight(-halfPower);
+			wait1Msec(500);
+			turnRight90();
+		}
+
+		// red is 27
+    if (SensorValue(lightSensor) < 30)
+      foundBlack = true;
+
+    if (foundBlack && SensorValue(lightSensor) >= 28)
+    {
+      foundBlack = false;
+      lineCount++;
+    }
+
+    displayTextLine(5, "     line#: %d", lineCount);
+
+    if (lineCount == 1)
+    {
+  		driveStraight(fullStop);
+  		break; // exit the current loop
+  	}
+	}
+}
