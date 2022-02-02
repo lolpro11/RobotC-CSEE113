819fd9351b40d5d2110d21c317602ad914078301
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2022-02-02 08:42:35 -0800

    Update robo500.c

--- robo500.c
+++ robo500.c
@@ -3,15 +3,21 @@
 task main() {
     displayCenteredTextLine(3, "I use Arch BTW.");
     for(int repeats = 0; repeats <= 8; repeats++) {
-
-        motor[motorB] = 100;
-        motor[motorC]  = 100;
-        displayTextLine(0, "Moving Forward.");
-        wait1Msec(2100);
-
-        motor[motorB] = -100;
-        motor[motorC] = 100;
-        displayTextLine(0, "Turning Right.");
-        wait1Msec(386);
+        while(true) {
+            if (SensorValue(touchSensor) == 0) {
+                motor[motorB] = 100;
+                motor[motorC]  = 100;
+                displayTextLine(0, "Moving Forward.")
+            } 
+            else {
+                motor[motorB] = -100;
+                motor[motorC]  = -100;
+                wait1Msec(500);
+                motor[motorB] = -100;
+                motor[motorC] = 100;
+                displayTextLine(0, "Turning Right.");
+                wait1Msec(386);
+            }
+        }
     }
 }
